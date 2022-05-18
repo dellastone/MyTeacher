905c412011081f4b1c5361cc349b16ad9252dc93
Daniele Della Pietra <daniele.dellapietra@studenti.unitn.it>
2022-05-18 11:14:53 +0200

    auth update

--- app/authentication.js
+++ app/authentication.js
@@ -4,59 +4,45 @@ const User = require('../db_connection/models/user'); // get our mongoose model
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 
-router.post('', async function(req, res) {
-	
-	// find the user
-	let user = await User.findOne({
-		email: req.body.email
-	}).exec();
-	
-	// user not found
-	if (!user) {
-		//test
-		const User = require("../db_connection/models/user")
-		let user = new User({
-			username: "marioR",
-			nome: "Mario",
-			cognome: "Rossi",
-			professore: false,
-			email: req.body.email,
-			phone: "3122222112",
-			//image: Image,
-			password: req.body.password,
-			materie: [""],
-			argomenti: [""],
-			prezzo: 0
-		});
-		user = await user.save();
-		//test
-		return res.json({ success: false, message: 'Errore, utente non trovato!' });
-	}
-	
-	// check if password matches
-	if (user.password != req.body.password) {
-		return res.json({ success: false, message: 'Errore, password errata!' });
-	}
-	
-	// if user is found and password is right create a token
-	var payload = {
-		email: user.email,
-		id: user._id
-		// other data encrypted in the token	
-	}
-	var options = {
-		expiresIn: 86400 // expires in 24 hours
-	}
-	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
-	res.json({
-		success: true,
-		message: 'Enjoy your token!',
-		token: token,
-		email: user.email,
-		id: user._id,
-		self: "api/v1/" + user._id
-	});
+router.post('', async function (req, res) {
+	try {
+		//Trova l'uente nel DB
+		let user = await User.findOne({
+			email: req.body.email
+		}).exec();
+
+		//L'utente non è stato trovato
+		if (!user) {
+			return res.status(400).json({ message: 'Errore, utente non trovato.' });
+		}
+
+		//controllo password
+		if (!user.validPassword(req.body.password)) {
+			return res.status(400).json({ message: 'Errore, password errata.' });
+		}
+
+		//crea il token per l'utente
+		var payload = {
+			email: user.email,
+			id: user._id,
+			username: user.username
+
+		}
+		var options = {
+			expiresIn: 3600
+		}
+		var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+		//Login effettuato
+		res.status(200).json({
+			message: 'Login effettuato.',
+			token: token,
+			self: "api/v1/users/" + user.username
+		});
+	} catch (err) {
+		return res.status(500).json({ message: 'Errore interno al Server.' });
+	}
 
 });
 
--- db_connection/models/user.js
+++ db_connection/models/user.js
@@ -1,17 +1,52 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var mongoose = require('mongoose');
+const crypto = require('crypto');
+var Schema = mongoose.Schema;
+
 // mongoose model that desribes the user of the platform
-module.exports = mongoose.model('user', new Schema({ 
-    username: String,
-    nome: String,
-    cognome: String,
-    professore: Boolean,
-	email: String,
+const UserSchema = mongoose.Schema({ 
+    username: {
+        type: String,
+        required: true
+    },
+    nome: {
+        type: String,
+        required: true
+    },
+    cognome: {
+        type: String,
+        required: true
+    },
+    indirizzo: String,
+    professore: {
+        type: Boolean,
+        required: true
+    },
+	email: {
+        type: String,
+        required: true
+    },
     phone: String,
-    //image: Image,
-	password: String,
+    image: String,
+	passwordHash: String,
+    salt: String,
     materie: [String],
     argomenti: [String],
     prezzo: Number
-}));
+});
+
+//crea un salt per la password unico per l'utente scelto, cripta la password dell'utente
+UserSchema.methods.setPassword = function (password){
+    this.salt = crypto.randomBytes(16).toString('hex'); 
+    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex'); 
+};
+
+//verifica se la password inserita è uguale a quella dell'utente calcolandone l'hash
+UserSchema.methods.validPassword = function(password) { 
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex'); 
+    return this.passwordHash === hash; 
+}; 
+
+module.exports = mongoose.model('Users', UserSchema)
