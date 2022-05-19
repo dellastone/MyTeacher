68c18e1a3804e8ff0994dd1a00667802376b1d90
nicolacasagrande-unitn <nicola.casagrande@studenti.unitn.it>
2022-05-19 11:17:08 +0200

    added cors and bigger payload

--- app/app.js
+++ app/app.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const authentication = require('./authentication.js');
 const expressSanitizer = require('express-sanitizer');
 const registration = require('../db_connection/registration');
 
 //configuring expressJS middleware
-app.use(express.json());
+app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(expressSanitizer());
+app.use(cors());
 
 app.use('/', express.static('static'));
 app.use('/api/v1/users/auth', authentication);
