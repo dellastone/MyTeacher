8f5ff2f5f5054bc506ef24cf7cc691bf4679ff77
dellastone <daniele.dellapietra@studenti.unitn.it>
2022-05-26 16:22:02 +0200

    add comments

--- app/checkLogin.js
+++ app/checkLogin.js
@@ -4,31 +4,36 @@ const jwt = require("jsonwebtoken");
 const User = require('../db_connection/models/user');
 
 
-
+//controlla lo stato di login dell'utente
 router.post('', async function (req, res) {
     try {
+        
         const token = req.sanitize(req.body.token);
+        //se il token è vuoto l'utente non ha effettuato il login
         if (token == null || token == undefined) {
             return res.status(200).send({ login:false});
         }
         try {
+            //decodifica il token
             const decoded = jwt.verify(token, config.SUPER_SECRET);
-            
+            //trova l'utente loggato
             const usr = await User.findOne({
                 username: decoded.username
             }).exec();
+            //se l'utente non esiste fai rieffettuare il login
             if(!usr)
                 return res.status(200).send({ login:false});
             else{
+                //ritorna username immagine e stato login a true
                 let usrname = usr.username;
                 let img = usr.image;
                 return res.status(200).send({ login:true, username: usrname, image: img});
             }
 
         } catch (err) {
+            //se il token è errato restituisci errore
             return res.status(400).send({ message: "Token errato" });
         }
-        return next();
     } catch (err) {
         console.log(err)
         return res.status(500).json({ message: 'Errore interno al Server.' });
