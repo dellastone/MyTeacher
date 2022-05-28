0a1280ebb24206b3c7dc07ee58cb482e9184301b
nicolacasagrande-unitn <nicola.casagrande@studenti.unitn.it>
2022-05-28 15:49:42 +0200

    transaction schema

---
+++ db_connection/models/transaction.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const TransactionSchema = mongoose.Schema({
+    mittente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }, 
+    ricevente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true 
+    },
+    ricarica: {
+        type: Boolean,
+        required: true
+    },
+    valore: {
+        type: Number,
+        required: true
+    },
+    lezione: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lection"
+    }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
