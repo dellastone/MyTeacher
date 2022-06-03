dae9070dbf8bebbf84bfb9c42dd24d33d15b230c
dellastone <daniele.dellapietra@studenti.unitn.it>
2022-06-03 12:25:23 +0200

    fix ui

--- static/script_ricarica.js
+++ static/script_ricarica.js
@@ -5,11 +5,7 @@ function ricarica() {
     amount = document.getElementById("amount").value
     cardOwner = document.getElementById("owner").value
    
-    if(cardOwner === ""){
-        document.getElementById("hidden_div").setAttribute("class", "show_div");
-        document.getElementById("alert_msg").innerHTML = "Inserisci proprietario carta";
-        return
-    }
+
     jsonToSend = '{"cardNumber":"' +cardNumber+ '","expirationDate":"'+expirationDate+'","cvv":"'+cvv+'","amount": "'+amount+'"}';
 
     fetch('../api/v2/ricarica', {
@@ -21,8 +17,9 @@ function ricarica() {
         .then(r => r.json().then(data => ({ status: r.status, body: data })))
         .then(obj => {
             msg = obj.body.message;
-            if (obj.status == 200) {
-                $("#success").modal('toggle');
+            if (obj.status == 200 && cardOwner === "") {
+                document.getElementById("hidden_div").setAttribute("class", "show_div");
+                document.getElementById("alert_msg").innerHTML = "Inserisci proprietario carta";
 
             } else if (obj.status == 400) {
                 document.getElementById("hidden_div").setAttribute("class", "show_div");
@@ -32,6 +29,9 @@ function ricarica() {
                 document.getElementById("hidden_div").setAttribute("class", "show_div");
                 document.getElementById("alert_msg").innerHTML = msg;
 
+            }else if(obj.status == 200 ){
+                
+                $("#success").modal('toggle');
             }
         })
         .catch(error => console.error(error));
